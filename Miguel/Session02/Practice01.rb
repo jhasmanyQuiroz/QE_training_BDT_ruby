3ab6b04d9f52b8ff8eead9ac1f1ff225a8a67e78
Admin <Admin>
2017-03-15 22:31:38 +0000

    Session02

---
+++ Miguel/Session02/Practice01.rb
@@ -0,0 +1,30 @@
+=begin
+
+
+
+print "Hello"
+puts "#{1 + 2}"
+puts 1 + 2
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+name = "Miguel"
+puts "Hello #{name}"
+=end
+
+int = 100 + 100 / 2
+string = "Hello" + 1.to_s
+float = 1.5
+
+#pepe
+
+puts "int: #{int}"
+puts "string: #{string}"
+puts "float: #{float}"
+
+
+
+manzanas = "manzanas "
+numeroManzanas = 10
+puts manzanas + numeroManzanas.to_s
+
+puts 1==1
