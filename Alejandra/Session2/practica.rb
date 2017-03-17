6cd3fb846b57e693c4e4d57571914a6bcc5b3941
aarteaga <Alejandra Arteaga@jalasoft.local>
2017-03-17 14:29:00 -0400

    added practica 2

---
+++ Alejandra/Session2/practica.rb
@@ -0,0 +1,5 @@
+seconds = 60.0
+result = 0.0
+puts "#{seconds} seconds to #{result = seconds/60.0} min"
+puts "#{seconds} seconds to #{result /= 60.0} hours"
+puts "#{seconds} seconds to #{result /= 3600} day"
