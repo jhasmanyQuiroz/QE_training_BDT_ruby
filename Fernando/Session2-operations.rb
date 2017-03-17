11072318a8d1e36780f9bc7434acdfa84fe9eeaf
Fernando Gutierrez <Fernando Gutierrez@jalasoft.local>
2017-03-17 11:59:15 -0400

    Task for session 2 it is done

---
+++ Fernando/Session2-operations.rb
@@ -0,0 +1,14 @@
+seconds=998555
+result_minutes=seconds/60
+result_hours=(seconds_to_minutes/60)
+result_days=((seconds_to_minutes/60)/60)/24
+
+puts "Seconds: #{seconds} to Minutes is: #{result_minutes}"
+
+puts "Seconds: #{seconds} to Hours is #{result_hours}"
+
+puts "Seconds: #{seconds} to Days is #{result_days}"
+
+
+
+
