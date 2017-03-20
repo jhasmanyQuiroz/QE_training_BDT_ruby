dff46149085eec2c68362b8b531336d1dcf8e563
aarteaga <Alejandra Arteaga@jalasoft.local>
2017-03-20 19:03:02 -0400

    updated session3 practice

--- Alejandra/Session3/practica2.rb
+++ Alejandra/Session3/practica2.rb
@@ -4,19 +4,18 @@ class Father
 		@@money
 	end 
 end 
+puts Father.money
 class Child1 < Father
 	@@money = @@money - 10 
 end
-
+puts Child1.money
 class Child2 < Father
 	@@money = @@money - 10
 end
-
+puts Child2.money
 class Child3 < Father
 	@@money = @@money - 10 
 end
-
-puts Father.money
-puts Child1.money
-puts Child2.money
 puts Child3.money
+puts Father.money
+
