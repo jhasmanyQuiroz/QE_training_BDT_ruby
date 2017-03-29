0cc2629d61ecc61b333f5626b55d7f42be361329
Admin <Admin>
2017-03-29 23:47:42 +0000

    exam Ruby

---
+++ Igor/exam/conversor.rb
@@ -0,0 +1,13 @@
+module Conversor
+   def mm_to_cm distance
+   	distance/10
+   end
+
+   def cm_to_mts distance
+   	distance/100
+   end
+
+   def mts_to_kilometer distance
+   	distance/1000
+   end
+end
